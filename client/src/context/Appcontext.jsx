3ad3724f40a5a29b08446425d7179f7de6ac3f8c
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2025-08-08 10:59:53 +0530

    updated

--- client/src/context/Appcontext.jsx
+++ client/src/context/Appcontext.jsx
@@ -4,10 +4,16 @@ import { useNavigate } from "react-router-dom";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({children})=>{
+
     const navigate = useNavigate();
-    const [user, setUser] = useState(null)
+
+    const [user, setUser] = useState(null)     // User state to manage authentication
+    
     const [isSeller, setIsSeller]= useState(false)
-    const value = {navigate,user, setuser, setIsSeller, isSeller}
+    const [showUserLogin, setShowUserLogin]= useState(false)
+
+
+    const value = {navigate,user, setUser, setIsSeller, isSeller,showUserLogin, setShowUserLogin}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'   
+
+const home = () => {
+  return (
+    <div className='mt-10'>
+        <MainBanner />
+    </div>
+  )
+}
+
+export default home
