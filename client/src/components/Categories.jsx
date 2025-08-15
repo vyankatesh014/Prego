89801f8344fe55c51158b5663a96ad3260dc14ea
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2025-08-15 23:31:05 +0530

    update

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -11,7 +11,7 @@ const Categories = () => {
     return (
         <div className=' mt-16'>
             <p className='text-2xl md:text-3xl font-medium'>categories</p>
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols- mt-6 gap-6'>
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6'>
 
                 {categories.map((category, index) => (
                     <div key={index} className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center'
@@ -19,9 +19,7 @@ const Categories = () => {
                         onClick={() => {
                             navigate(`/products/${category.path.toLowerCase()}`);
                             scrollTo(0, 0)
-                        }}
-
-                    >
+                        }} >
                         <img src={category.image} alt='{category.text}'
                             className='group-hover:scale-108 transition max-w-28' />
                         <p className='text-sm font-medium'>{category.text}</p>
@@ -32,4 +30,4 @@ const Categories = () => {
     )
 }
 
-export default categories
+export default Categories
